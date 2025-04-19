9afb7b58d1d2fbfbd055f62f0ae400d0acd1a207
AndrewScripter587 <145153158+AndrewScripter587@users.noreply.github.com>
2025-04-19 16:36:41 -0400

    Change some names and stuff

--- src/client/java/com/andrewgaming/APUtilsClient.java
+++ src/client/java/com/andrewgaming/APUtilsClient.java
@@ -2,7 +2,7 @@ package com.andrewgaming;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class AllowPlayerDataModificationClient implements ClientModInitializer {
+public class APUtilsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
